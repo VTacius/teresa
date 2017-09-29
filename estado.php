c301049f0a2955ca9bc3af961c628a9ae8de0e9d
Alexander Ortíz <vtacius@gmail.com>
2017-09-29 10:58:51 -0600

    El primer estado funcional

---
+++ estado.php
@@ -0,0 +1,57 @@
+<?php
+
+/* Obtenemos los datos desde nuestra fuente primaria: Por ahora, la API de libreNMS */
+$curl = curl_init();
+require_once('config.php');
+curl_setopt_array($curl, array(
+    CURLOPT_URL => 'http://' . $sapi . '/api/v0/devices',
+    CURLOPT_HTTPHEADER => ['X-Auth-Token: ' . $token],
+    CURLOPT_RETURNTRANSFER => true
+));
+
+$respuesta = curl_exec($curl);
+curl_close($curl);
+
+$datos = json_decode($respuesta, true);
+
+/* Acá guardaremos los datos, la salida final que hemos de enviar al cliente web */
+$salida = array();
+
+/* De la descripción en la base de datos, hacemos {tipo} - {descripción} */
+function parsea_descripcion ($descripcion){
+    if(preg_match('/^(\w+)\s\-\s(.+)/', $descripcion, $res) === 1){
+        return [$res[1], $res[2]];
+    } else {
+        return false;
+    }
+
+}
+
+/* De la localidad en la base de datos, hacemos [{latitude},{longitude}] */
+function parsea_localidad ($localidad){
+    if(preg_match('/\[(.+),(.+)\]/', $localidad, $res) === 1){
+        return [$res[1], $res[2]];
+    } else {
+        return false;
+    }
+}
+
+/* Procesa los dispositivos y crea un diccionario con los datos que en verdad necesitemos */
+foreach($datos['devices'] as $d){
+    list($latitude, $longitude) = parsea_localidad($d['location']);
+    list($tipo, $nombre) = parsea_descripcion($d['purpose']);
+    if (is_string($latitude) and is_string($tipo)){
+    	$salida[] = [ 
+    	    'tipo' => $tipo,
+    	    'nombre' => $nombre,
+    	    'latitude' => $latitude,
+    	    'longitude' => $longitude,
+    	    'estado' => $d['status'],
+    	    'duracion' => $d['last_polled']
+    	];
+        
+    }
+}
+
+print json_encode($salida); 
+?>
