9433c30a7bc9a5e075d4550b42092faf5f7a354f
Alexander Ortíz <vtacius@gmail.com>
2022-09-20 17:47:11 -0600

    Pues como prueba de concepto, ya es funcional según nuestros requerimientos actuales

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # status-map
 Un sencillo script para verificar el estado de dispositivos en base a los datos de LibreNMS
+
+# 
+run --name servidor -v $PWD/docker/default.conf:/etc/nginx/conf.d/default.conf:ro -v $PWD/public:/usr/share/nginx/html/ --rm -it -p 8000:80 nginx
--- src/main.rs
+++ src/main.rs
@@ -27,12 +27,6 @@ fn recibir_mensaje(canal: Canal) -> impl Stream<Item = Result<Event, warp::Error
     rx.map(|dispositivos| {Ok(Event::default().json_data(dispositivos).unwrap())})
 }
 
-fn inicio () -> Result<warp::hyper::Response<&'static str>, warp::http::Error>{
-    warp::http::Response::builder()
-        .header("content-type", "text/html; charset=utf-8")
-        .body(INDEX_HTML)
-}
-
 #[tokio::main]
 async fn main() {
 
@@ -58,71 +52,10 @@ async fn main() {
             warp::sse::reply(event_stream)
         });
     
-    let index = warp::path::end().map(inicio);
 
-    let routes = index.or(buzon).or(cartero);
+    let routes = buzon.or(cartero);
    
     warp::serve(routes)
-        .run(([127, 0, 0, 1], 8080)).await;
+        .run(([0, 0, 0, 0], 8080)).await;
 
 }
-
-static INDEX_HTML :&str = r#"
-<!DOCTYPE html>
-<html>
-    <head>
-        <title> Mapa de Estado para dispositivos </title>
-        <link rel="stylesheet" href="https://unpkg.com/modern-css-reset/dist/reset.min.css" />
-        <link rel="stylesheet" href="https://unpkg.com/leaflet@1.8.0/dist/leaflet.css" integrity="sha512-hoalWLoI8r4UszCkZ5kL8vayOGVae1oxXe/2A4AO6J9+580uKHDO3JdHb7NzwwzK5xr/Fs0W40kiNHxM9vyTtQ==" crossorigin="" />
-        <style type="text/css">
-            body {
-                height: 600px;
-            }
-            .mapa {
-                height: 590px;
-            }
-            
-        </style>
-    </head>
-    <body>
-        <div id="mapa"></div>
-    </body>
-    <script src="https://unpkg.com/leaflet@1.8.0/dist/leaflet.js" integrity="sha512-BB3hKbKWOc9Ez/TAwyWxNXeoV9c1v6FIeYiBieIWkpLjauysF18NzgR1MBNBXf8/KABdlkX68nAhlwcDFLGPCQ==" crossorigin=""></script>
-    <script type="text/javascript">
-        // Los valores por defecto para el mapa
-        var default_lat = "13.8054";
-        var default_lng = "-88.9069";
-        var default_zoom = 10;
-
-        /**  Esto es básicamente la configuración inicial del mapa */
-        var mapa = L.map('mapa').setView([default_lat, default_lng], default_zoom);
-
-        /** Y esto hará que aparezca por arte de magia */
-        L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            attribution: '&copy; <a href="http://osm.org/copyright">OpenStreetMap</a> contributors'
-        }).addTo(mapa);
-
-        
-        /** Aca empezamos con los datos */
-        let uri = 'http://' + location.host + '/buzon';
-        let sse = new EventSource(uri);
-            
-        function mostrarEstablecimientos(data) {
-            console.log(data);
-            let contenido = JSON.parse(data);
-            console.log(contenido);
-            contenido.forEach(point => {
-                L.marker([point.latitude, point.longitude]).addTo(mapa)
-                    .bindPopup('A pretty CSS3 popup.<br> Easily customizable.');
-                });
-            
-
-        }
-
-        sse.onmessage = function(mensaje) {
-            mostrarEstablecimientos(mensaje.data);
-        }
-
-    </script>
-</html>
-"#;
